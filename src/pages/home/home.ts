4d07589677195822fa3efc4385634872b6457479
darpanvithani <vithanidarpan@gmail.com>
2018-05-11 11:13:14 +0530

    Country picker implemented using static data.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,44 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import {PhoneVerificationPage} from "../phone-verification/phone-verification";
+import {ModalController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  // logoImage : string = "../../assets/imgs/ZTtYkKtCSJulRyheRIxB_Special-request-512.png";
+  logoImage: string = "../../assets/imgs/home.png";
+  isListOpen: boolean;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
 
   }
 
-  login()
-  {
+  login() {
     console.log("yes");
     this.navCtrl.push(PhoneVerificationPage);
+  }
+
+  ionViewDidLoad() {
+    this.isListOpen = false;
+  }
+
+  selectCountry() {
+    this.isListOpen = !this.isListOpen;
+  }
+
+  chooseCountry(){
+    this.isListOpen = !this.isListOpen;
+  }
+
+  public openModal() {
+
+  }
+
+  public closeModal() {
+
   }
 
 }
