4bb7250c0a7e3fc9adaa002a90b3160b948d9521
brijesh patel <brijeshptl62@gmail.com>
2018-05-18 10:17:10 +0530

    Set phone number example as placeholder is implemented

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -37,30 +37,39 @@ export class HomePage {
 
     this.http.get("https://api.ipdata.co").subscribe(data => {
       var countryCode: any = (data.country_code).toLowerCase();
-      self.activeFlag = 'flag-icon-'+countryCode;
+      self.activeFlag = 'flag-icon-' + countryCode;
       var activeCountryArray: any;
       activeCountryArray = this.allCountries.filter((obj) => {
         return obj.flag === this.activeFlag;
       });
       this.activeCountry = activeCountryArray[0];
-      console.log(this.activeCountry )
+      if (this.activeCountry.numberExample) {
+        this.numberPlaceholder = this.activeCountry.numberExample;
+      } else {
+        this.numberPlaceholder = "Mobile Number";
+      }
     }, error => {
       console.log(JSON.stringify(error.json()));
       self.activeFlag = 'flag-icon-us';
     });
 
-    if(!this.activeFlag) {
+    if (!this.activeFlag) {
       this.activeFlag = 'flag-icon-us';
     }
     this.countryService.getAllCountry().then(
       (success) => {
         self.allCountries = success;
         var activeCountryArray: any;
-        if(!this.activeFlag) {
+        if (!this.activeFlag) {
           activeCountryArray = this.allCountries.filter((obj) => {
             return obj.flag === this.activeFlag;
           });
           this.activeCountry = activeCountryArray[0];
+          if (this.activeCountry.numberExample) {
+            this.numberPlaceholder = this.activeCountry.numberExample;
+          } else {
+            this.numberPlaceholder = "Mobile Number";
+          }
         }
       }).catch(
       (err) => {
@@ -80,26 +89,26 @@ export class HomePage {
     this.mobile = null;
 
     var self: any = this;
-    document.addEventListener ("keydown", function (zEvent) {
+    document.addEventListener("keydown", function (zEvent) {
 
       var selectedCountry: any = [];
       for (var x in self.allCountries) {
-        if((((self.allCountries[x].name).substring(0,1)) == (zEvent.key).toUpperCase())) {
+        if ((((self.allCountries[x].name).substring(0, 1)) == (zEvent.key).toUpperCase())) {
           selectedCountry.push(self.allCountries[x].flag);
           break
         }
       }
       self.selectedCountry = selectedCountry;
-      if(self.selectedCountry.length > 0) {
+      if (self.selectedCountry.length > 0) {
         var targetLi: any = document.getElementById(self.selectedCountry);
         targetLi.scrollIntoView(((targetLi.offsetTop) / 4) - 50);
       }
-    } );
+    });
 
   }
 
   validationCheckFn() {
-    if(this.mobile && (/^\d+$/.test(this.mobile))) {
+    if (this.mobile && (/^\d+$/.test(this.mobile))) {
       const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
       const number = phoneUtil.parseAndKeepRawInput(this.mobile, this.activeCountry.countryCode);
 
@@ -116,7 +125,11 @@ export class HomePage {
   chooseCountry(country) {
     document.getElementById("flagUl").style.display = "none";
     this.activeCountry = country;
-    console.log(this.activeCountry)
+    if (this.activeCountry.numberExample) {
+      this.numberPlaceholder = this.activeCountry.numberExample;
+    } else {
+      this.numberPlaceholder = "Mobile Number";
+    }
     this.isListOpen = !this.isListOpen;
     this.activeFlag = country.flag;
   }
